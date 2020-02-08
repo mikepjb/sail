7eaf85a622b5ba61025b498e1a2bd69e2546c227
Michael Bruce <me@michaelbruce.co>
2020-02-08 23:07:05 +0000

    added a good amount of border classes

--- src/sail/components.clj
+++ src/sail/components.clj
@@ -42,7 +42,81 @@
            :border-transparent {:border-collapse "transparent"}]
           (color-class "border" "border-color")
           (with-pseudo-class "hover" (color-class "border" "border-color"))
-          (with-pseudo-class "focus" (color-class "border" "border-color"))))
+          (with-pseudo-class "focus" (color-class "border" "border-color"))
+          [:rounded-none {:border-radius "none"}
+           ;; TODO rounded-sm is based on sm being a particular value
+           ;; treat like color with palette?
+           :rounded-sm {:border-radius "0.125rem"}
+           :rounded {:border-radius "0.25rem"}
+           :rounded-lg {:border-radius "0.5rem"}
+           :rounded-full {:border-radius "9999px"}
+           :rounded-t-none {:border-top-left-radius 0
+                            :border-top-right-radius 0}
+           :rounded-b-none {:border-bottom-left-radius 0
+                            :border-bottom-right-radius 0}
+           :rounded-l-none {:border-top-left-radius 0
+                            :border-bottom-left-radius 0}
+           :rounded-r-none {:border-top-right-radius 0
+                            :border-bottom-right-radius 0}
+           :rounded-t-sm {:border-top-left-radius "0.125rem"
+                          :border-top-right-radius "0.125rem"}
+           :rounded-b-sm {:border-bottom-left-radius "0.125rem"
+                          :border-bottom-right-radius "0.125rem"}
+           :rounded-l-sm {:border-top-left-radius "0.125rem"
+                          :border-bottom-left-radius "0.125rem"}
+           :rounded-r-sm {:border-top-right-radius "0.125rem"
+                          :border-bottom-right-radius "0.125rem"}
+           :rounded-t {:border-top-left-radius "0.25rem"
+                       :border-top-right-radius "0.25rem"}
+           :rounded-b {:border-bottom-left-radius "0.25rem"
+                       :border-bottom-right-radius "0.25rem"}
+           :rounded-l {:border-top-left-radius "0.25rem"
+                       :border-bottom-left-radius "0.25rem"}
+           :rounded-r {:border-top-right-radius "0.25rem"
+                       :border-bottom-right-radius "0.25rem"}
+           :rounded-t-lg {:border-top-left-radius "0.5rem"
+                          :border-top-right-radius "0.5rem"}
+           :rounded-b-lg {:border-bottom-left-radius "0.5rem"
+                          :border-bottom-right-radius "0.5rem"}
+           :rounded-l-lg {:border-top-left-radius "0.5rem"
+                          :border-bottom-left-radius "0.5rem"}
+           :rounded-r-lg {:border-top-right-radius "0.5rem"
+                          :border-bottom-right-radius "0.5rem"}
+           :rounded-t-full {:border-top-left-radius "9999px"
+                            :border-top-right-radius "9999px"}
+           :rounded-b-full {:border-bottom-left-radius "9999px"
+                            :border-bottom-right-radius "9999px"}
+           :rounded-l-full {:border-top-left-radius "9999px"
+                            :border-bottom-left-radius "9999px"}
+           :rounded-r-full {:border-top-right-radius "9999px"
+                            :border-bottom-right-radius "9999px"}
+           :rounded-tl-none {:border-top-left-radius "0"}
+           :rounded-tr-none {:border-top-right-radius "0"}
+           :rounded-bl-none {:border-bottom-left-radius "0"}
+           :rounded-br-none {:border-bottom-right-radius "0"}
+           :rounded-tl-sm {:border-top-left-radius "0.125rem"}
+           :rounded-tr-sm {:border-top-right-radius "0.125rem"}
+           :rounded-bl-sm {:border-bottom-left-radius "0.125rem"}
+           :rounded-br-sm {:border-bottom-right-radius "0.125rem"}
+           :rounded-tl {:border-top-left-radius "0.25rem"}
+           :rounded-tr {:border-top-right-radius "0.25rem"}
+           :rounded-bl {:border-bottom-left-radius "0.25rem"}
+           :rounded-br {:border-bottom-right-radius "0.25rem"}
+           :rounded-tl-lg {:border-top-left-radius "0.5rem"}
+           :rounded-tr-lg {:border-top-right-radius "0.5rem"}
+           :rounded-bl-lg {:border-bottom-left-radius "0.5rem"}
+           :rounded-br-lg {:border-bottom-right-radius "0.5rem"}
+           :rounded-tl-full {:border-top-left-radius "9999px"}
+           :rounded-tr-full {:border-top-right-radius "9999px"}
+           :rounded-bl-full {:border-bottom-left-radius "9999px"}
+           :rounded-br-full {:border-bottom-right-radius "9999px"}
+           :border-solid {:border-style "solid"}
+           :border-dashed {:border-style "dashed"}
+           :border-dotted {:border-style "dotted"}
+           :border-double {:border-style "double"}
+           :border-none {:border-style "none"}
+           ; L3226 border-0
+           ]))
 
 ;; (defn with-media-query)
 (defn with-pseudo-class
---
+++ src/sail/size.clj
@@ -0,0 +1,17 @@
+(ns sail.size)
+
+;; Sample from L3010
+;; .rounded-sm {
+;;   border-radius: 0.125rem;
+;; }
+;; 
+;; .rounded {
+;;   border-radius: 0.25rem;
+;; }
+;; 
+;; .rounded-lg {
+;;   border-radius: 0.5rem;
+;; }
+
+;; for pixels only
+;; (def scale {:sm "0.125rem"})
