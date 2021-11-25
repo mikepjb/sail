ed8a78c59f5cf656dd202d380cf5043022cb050c
Mike Bruce <mike@hypalynx.com>
2021-11-25 03:04:59 +0000

    allow exceptions for vendor rules

--- test/clj/sail/validation_test.clj
+++ test/clj/sail/validation_test.clj
@@ -13,6 +13,17 @@
        (.getBytes encoding)
        (java.io.ByteArrayInputStream.))))
 
+(def allowed-properties
+  ["-webkit-text-size-adjust"
+   "-webkit-box-pack" ;; webkit ext. for box-pack
+   "-webkit-box-flex" ;; webkit ext. for box-flex
+   "-webkit-box-align" ;; webkit ext. for box-align
+   "-webkit-box-ordinal-group" ;; webkit ext. for box-align
+   "-webkit-box-orient" ;; webkit ext. for box-align
+   "-webkit-box-direction" ;; webkit ext. for box-align
+   ;; "::-moz-focus-inner" ;; this isn't a property..
+   ])
+
 (defn validate [css-text]
   (let [filename "file://localhost/unused-parameter.css"
         ac (new ApplContext "en")
@@ -22,12 +33,19 @@
         _ (.parseStyleElement ssp ac input-stream nil usermedium (new URL filename) 0)
         stylesheet (.getStyleSheet ssp)
         _ (.findConflicts stylesheet ac)
-        error-count (.getErrorCount (.getErrors stylesheet))]
+        initial-error-count (.getErrorCount (.getErrors stylesheet))
+        errors (map (fn [n]
+                    (let [exception-class (.getSimpleName (.getClass (.getException (.getErrorAt (.getErrors stylesheet) n))))]
+                      (if (= exception-class "CssParseException")
+                        (when-not (some #{(.getProperty (.getException (.getErrorAt (.getErrors stylesheet) n)))} allowed-properties)
+                          (.getMessage (.getException (.getErrorAt (.getErrors stylesheet) n))))
+                        (.getMessage (.getException (.getErrorAt (.getErrors stylesheet) n)))
+                      )))
+                    (take 100 (range 0 error-count)))
+        error-count (count errors)]
 
     {:error-count error-count
-     :errors (map (fn [n]
-                    (.getMessage (.getException (.getErrorAt (.getErrors stylesheet) n))))
-                    (range 0 error-count))}))
+     :errors errors}))
 
 (deftest generated-css-correctness
   (testing "the unpurged css we generate is valid according to the w3c css validator"
@@ -36,8 +54,8 @@
       
       (is (= (:error-count results) 0)))))
 
-(comment
+;; (comment
   (def css-text (core/internal-generate-styles core/all components/components))
-  (doseq [n (take 20 (:errors (validate css-text)))]
+  (doseq [n (:errors (validate css-text))]
     (prn n))
-  )
+  ;; )
